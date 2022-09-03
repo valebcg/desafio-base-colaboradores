6862890da320470aa6cbfe32b6ef9d1e5c4bbf34
valebcg <valeria.cortes.g@gmail.com>
2022-09-02 22:30:17 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { baseColaboradores } from "./baseColaboradores";
 
 function App() {
+  const [array, setArray]= useState(baseColaboradores)
+  const [colaborador, setColaborador] = useState("");
+  const [email, setEmail]= useState ("")
+  const [name, setName]= useState ("")
+  
+const captureInput= (e) => {
+  setArray([
+    ...array,
+{nombre: name,correo: email},
+  ])
+      
+  setEmail("")
+  setName("")
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <div className="buscador">
+          <h1>Buscador de Colaboradores</h1>
+          <input
+            type="text"
+            placeholder="Buscar Colaborador"
+            className="search"
+            onChange={(e) => setColaborador(e.target.value)}
+          />
+        </div>
+        </div>
+
+<div className="container">
+        <input
+          type="text" 
+          className="input"
+          value={name}
+          placeholder="Ingrese el nombre del Colaborador"
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <input
+          type="email"
+          className="input"
+          value={email}
+          placeholder="Ingrese el correo del colaborador"
+          onChange={(e)  => setEmail(e.target.value)}
+        ></input>
+        <button onClick={captureInput}>Agregar</button>
+      
+
+      <h3>Listado de Colaboradores</h3>
+      <ul className="list">
+        {array.filter((user) => user.nombre.toLowerCase().includes(colaborador))
+          .map((user) => (
+            <li key={user.id} className="listItem">
+              {user.nombre} - {user.correo}
+            </li>
+          ))}
+      </ul>
+      </div>
+    </>
   );
 }
 
---
+++ src/baseColaboradores.js
@@ -0,0 +1,21 @@
+ export const baseColaboradores = [
+    {
+    id: "1",
+    nombre: "Sebastian Román",
+    correo: "sebastian@desafiolatam.com" 
+  
+    },
+   
+    {
+    id: "3",
+    nombre: "Oscar Acevedo",
+    correo: "oscar@desafiolatam.com" 
+    },
+   
+    {
+    id: "4",
+    nombre: "Juan Gonzalez",
+    correo: "juan@desafiolatam.com" 
+    },
+    ]
+   
